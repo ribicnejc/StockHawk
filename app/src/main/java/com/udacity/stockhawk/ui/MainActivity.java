73a9ea2810c7e040893eaf0869116285a924e553
Nejc Ribic <nejc.ribic95@gmail.com>
2017-05-02 22:12:43 +0200

    Creating widget

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.udacity.stockhawk.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -46,6 +47,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public void onClick(String symbol) {
+        Intent intent = new Intent(this, MoreInfoActivity.class);
+        startActivity(intent);
         Timber.d("Symbol clicked: %s", symbol);
     }
 
